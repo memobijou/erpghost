b9a36d0c30c325d34d4031f77a088ad7150300d7
mbijou <mbijou@live.de>
2017-12-11 02:35:52 +0100

    product list view

--- product/views.py
+++ product/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-#from .models import Product
+from .models import Product
 
 # Create your views here.
 
 class ProductListView(ListView):
     pass
-	#def get_queryset(self):
-		#queryset = Product.objects.all()
-		#return queryset
+	def get_queryset(self):
+		queryset = Product.objects.all()
+		return queryset

e488db39d007f4ecaa7df287d65ce41bdf6ed953
mbijou <mbijou@live.de>
2017-12-11 02:18:47 +0100

    product list view

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	ean = models.CharField(blank=True, null=False, max_length=13)
+
+	def __str__(self):
+		return self.ean
--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
 # Create your views here.
+
+class ProductListView(ListView):
+
+	def get_queryset(self):
+		queryset = Product.objects.all()
+		return queryset

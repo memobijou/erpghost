61ecd36e151eefbb753f5fb000cc3cf1e23de2cc
mbijou <mbijou@live.de>
2017-12-16 07:15:47 +0100

    added mission removed lib

--- erpghost/settings/base.py
+++ erpghost/settings/base.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lib',
+    # 'lib',
     'product',
     'order',
     'rest_framework',
+    'mission',
+
 ]
 
 MIDDLEWARE = [
--- erpghost/settings/production.py
+++ erpghost/settings/production.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lib',
+    # 'lib',
     'product',
     'order',
     'rest_framework',
+    'mission',
 
 ]
 

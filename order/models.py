d33bf588a865a062e4bd4ba3f69a963ccf1176f7
mbijou <mbijou@live.de>
2017-12-20 07:42:43 +0100

    added ProductOrder on model

--- order/models.py
+++ order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django.core.urlresolvers import reverse
+from product.models import Product
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Order(models.Model):
 	ordernumber = models.CharField(max_length=13)
 	delivery_date = models.DateField(default=datetime.date.today)
 	status = models.CharField(max_length=25, null=True, blank=True)
-
+	products = models.ManyToManyField(Product, through="ProductOrder")
 	#delivery_date = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
@@ -17,3 +18,10 @@ class Order(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("order:detail", kwargs={"pk": self.id})
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, unique=False, blank=False, null=False)
+    amount = models.IntegerField(null=False, blank=False, default=0)
+
+    confirmed = models.NullBooleanField()

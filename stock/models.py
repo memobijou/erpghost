e9172e1b68022df0fdc5736524195242c852da9a
Mimoun Bijjou <mimoun.bijjou@node.energy>
2018-01-24 00:57:05 +0100

    added ignore_unique to model, ignore unique when field is 'IGNORE'

--- stock/models.py
+++ stock/models.py
@@ -5,6 +5,11 @@ from django.core.exceptions import ValidationError
 
 
 class Stock(models.Model):
+    IGNORE_CHOICES = (
+        ('IGNORE', 'Ja'),
+        ('NOT_IGNORE', 'Nein'),
+    )
+
     ean_vollstaendig = models.CharField(max_length=250)
     bestand = models.IntegerField(null=True, blank=True)
     ean_upc = models.CharField(max_length=250, null=True, blank=True)
@@ -16,7 +21,8 @@ class Stock(models.Model):
     karton = models.CharField(max_length=250, null=True, blank=True)
     box = models.CharField(max_length=250, null=True, blank=True)
     aufnahme_datum = models.CharField(max_length=250, null=True, blank=True)
-    ignore_unique = models.CharField(max_length=250, null=True, blank=True)
+    ignore_unique = models.CharField(max_length=250, null=True, blank=True, choices=IGNORE_CHOICES,
+                                     verbose_name="Block")
 
     def get_absolute_url(self):
         return reverse("stock:list")
@@ -25,10 +31,11 @@ class Stock(models.Model):
         return str(self.ean_vollstaendig)
 
     def clean(self):
-        # Don't allow draft entries to have a pub_date.
+        if self.ignore_unique == "IGNORE":
+            return
+
         stocks = Stock.objects.filter(ean_vollstaendig=self.ean_vollstaendig, zustand=self.zustand,
                                       lagerplatz=self.lagerplatz)
-
         if stocks.count() > 0:
             raise ValidationError(_('Lagerbestand schon vorhanden'))
 

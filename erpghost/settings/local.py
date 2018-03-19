8f0cdbfe9d80aeaff50f29ecb193ea92f2cde287
mbijou <mbijou@live.de>
2018-03-19 22:41:37 +0100

    back to old settings style

--- erpghost/settings/local.py
+++ erpghost/settings/local.py
@@ -1,5 +1,4 @@
 from .base import *
 
-if os.environ.get('DJANGO_LOCAL'):
-    CELERY_BROKER_URL = 'redis://localhost:6379'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'

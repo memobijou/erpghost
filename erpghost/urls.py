ce4b1ba86380a52c275baaa6df699df7da972ef5
mbijou <mbijou@live.de>
2018-03-30 15:13:55 +0200

    if debug for static link in url

--- erpghost/urls.py
+++ erpghost/urls.py
@@ -44,5 +44,6 @@ urlpatterns = [
 ]
 
 #urlpatterns += staticfiles_urlpatterns()
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG is True:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

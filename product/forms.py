ef4d16486eaf6329bb448027b34432e1eb1a99c6
mbijou <mbijou@live.de>
2018-05-21 05:06:23 +0200

    purchasing prices not negative anymore

--- product/forms.py
+++ product/forms.py
@@ -31,11 +31,19 @@ class PurchasingPriceForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['purchasing_price'].widget.attrs['min'] = 0.01
+
         for visible in self.visible_fields():
             print(type(visible.field))
             if type(visible.field) is CharField or type(visible.field) is FloatField \
                     or type(visible.field) is IntegerField:
                 visible.field.widget.attrs["class"] = "form-control"
+
+    def clean_purchasing_price(self):
+        purchasing_price = self.cleaned_data['purchasing_price']
+        if purchasing_price is not None and purchasing_price < 0.01:
+            raise forms.ValidationError("Die Zahl muss größer als 0 sein")
+        return purchasing_price
 
 
 class ProductIcecatForm(ProductForm):

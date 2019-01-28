c08e324aa325c44c4317002e09e1abd92dfc14dc
mbijou <mimounbijjou@Mimouns-MBP.local>
2019-01-28 03:29:21 +0100

    docker support now

--- erpghost/settings/local.py
+++ erpghost/settings/local.py
@@ -20,16 +20,31 @@ SECURE_FRAME_DENY = False
 
 # DATABASE
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("PG_DB"),
+#         'USER': os.environ.get("PG_USER"),
+#         'PASSWORD': os.environ.get("PG_PASSWORD"),
+#         'HOST': os.environ.get("PG_HOST"),
+#         'PORT': os.environ.get("PG_PORT"),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("PG_DB"),
-        'USER': os.environ.get("PG_USER"),
-        'PASSWORD': os.environ.get("PG_PASSWORD"),
-        'HOST': os.environ.get("PG_HOST"),
-        'PORT': os.environ.get("PG_PORT"),
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "db",
+        'PORT': 5432,
     }
 }
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # DEBUG TOOLBAR
